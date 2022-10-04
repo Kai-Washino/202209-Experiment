fd69ee7ea0f48443ca4cd1904d2668405f297c5e
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2022-10-04 19:19:03 +0900

    make graph for Interection2022

--- make_graph.py
+++ make_graph.py
@@ -31,6 +31,7 @@ def main():
         del df['no need']
         result_list.append(df['after']-df['before'])
     player_num = len(csv_list) 
+
     make_linegraph_1part(result_list, player_num, turn_list, title_list)
     make_linegraph_4part(result_list, player_num, turn_list, title_list)
     make_bargraph(result_list, player_num, turn_list, title_list) 
@@ -41,6 +42,7 @@ def make_linegraph_1part(result_list, player_num, turn_list, title_list):
     plt.rcParams["font.size"] = 5
     fig_1part = plt.figure()
     subplot_area = []
+    all_data = []
     x = ['0', '5', '10', '15', '20', '25', '30', '35', '40']
     separete = player_num // 3 + 2
     # subplot_area.append(fig_1part.add_subplot(separete, separete, i+1))
@@ -48,19 +50,17 @@ def make_linegraph_1part(result_list, player_num, turn_list, title_list):
     turn2 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     turn3 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
     turn4 = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+    all_average = [0, 0, 0, 0, 0, 0, 0, 0, 0]
 
     for i in range(player_num):
         subplot_area.append(fig_1part.add_subplot(3, separete, i+1))
-
         y = []
         for j in range(9):
             y.append(result_list[i].iloc[j])
-        
+            print(result_list[i].iloc[j])
         odor_time = []
         for j in range(4):
             odor_time.append(turn_list[i].at[0,j])
-
         # print(odor_time)
         marker_name = ['','','','']
         for j in range(4):
@@ -85,23 +85,34 @@ def make_linegraph_1part(result_list, player_num, turn_list, title_list):
         # subplot_area[i].legend(loc = 'upper right')
         subplot_area[i].set_ylim(0, 0.65)
         plt.tick_params(labelsize=5)
-        print(odor_time[0])
         if(odor_time[0] == 5.0):
+            print("5")
             for i in range(9):
                 turn1[i] += y[i]
+                all_average[i] += y[i]
         elif(odor_time[0] == 60.0):
+            print("60")
             for i in range(9):
                 turn2[i] += y[i]
+                all_average[i] += y[i]
         elif(odor_time[0] == 10.0):
+            print("10")
             for i in range(9):
                 turn3[i] += y[i]
+                all_average[i] += y[i]
         else:
+            print("30")
             for i in range(9):
                 turn4[i] += y[i]
+                all_average[i] += y[i]
 
     for i in range(9):
-        turn1[i] = turn1[i]/player_num*2
-        turn2[i] = turn2[i]/player_num*2
+        turn1[i] = turn1[i]/player_num*3
+        turn2[i] = turn2[i]/player_num*3
+        turn3[i] = turn3[i]/player_num*3
+        turn4[i] = turn4[i]/player_num*3
+        all_average[i] = all_average[i]/player_num
+    
     
     marker = [5, 10, 30, 60]
     marker2 = [10, 60, 5, 30]
@@ -136,15 +147,35 @@ def make_linegraph_1part(result_list, player_num, turn_list, title_list):
     plt.tight_layout()
     plt.show()
 
+    plt.rcParams["font.size"] = 15
+    plt.rcParams['pdf.fonttype'] = 42
+    plt.rcParams['font.family'] = 'Meiryo'
+    plt.xlabel('時間 [分]', fontname='Meiryo', fontsize=20)
+    plt.ylabel('唾液分泌量 [g]', fontname='Meiryo', fontsize=20)
+    plt.ylim(0, 0.6)
+    plt.plot(x, turn1, lw=2, label = "5-10-30-60群")
+    plt.plot(x, turn2, lw=2, label = "60-30-10-5群")
+    plt.plot(x, turn3, lw=2, label = "10-60-5-30群")
+    plt.plot(x, turn4, lw=2, label = "30-5-60-10群")
+    plt.plot(x, all_average, lw=2, label = "全ての群の平均")
+    plt.legend(loc = 'upper left')
+    plt.tight_layout()
+    plt.show()
+
 def make_linegraph_4part(result_list, player_num, turn_list, title_list):
     #result_listは1列の表示するグラフ，player_numはプレイヤーの人数，turu_listはにおいを噴射した時間，title_listはタイトル名
 
+    plt.rcParams["font.size"] = 5
     x = ['0', '5', '10']
     average_turn1 = [0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]
     average_turn2 = [0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]
     average_turn3 = [0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]
     average_turn4 = [0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]
     average_all = [0, 0, 0],[0, 0, 0],[0, 0, 0],[0, 0, 0]
+    sec5_list = []
+    sec10_list = []
+    sec30_list = []
+    sec60_list = []
     fig_3part = plt.figure()
     subplot_area = []
     separete = player_num // 3 + 3
@@ -185,52 +216,76 @@ def make_linegraph_4part(result_list, player_num, turn_list, title_list):
         subplot_area[i].legend(loc = 'upper right')
         subplot_area[i].set_ylim(0, 0.65)
         if(odor_time[0] == 5.0):
+            sec5_list.append(y1)
+            sec10_list.append(y2)
+            sec30_list.append(y3)
+            sec60_list.append(y4)
             for i in range(3):
-                average_all[0][i] =+ y1[i]
-                average_all[1][i] =+ y2[i]
-                average_all[2][i] =+ y3[i]
-                average_all[3][i] =+ y4[i]
-                average_turn1[0][i] =+ y1[i]
-                average_turn1[1][i] =+ y2[i]
-                average_turn1[2][i] =+ y3[i]
-                average_turn1[3][i] =+ y4[i]
+                average_all[0][i] += y1[i]
+                average_all[1][i] += y2[i]
+                average_all[2][i] += y3[i]
+                average_all[3][i] += y4[i]
+                average_turn1[0][i] += y1[i]
+                average_turn1[1][i] += y2[i]
+                average_turn1[2][i] += y3[i]
+                average_turn1[3][i] += y4[i]
         elif(odor_time[0] == 60.0):
+            sec5_list.append(y4)
+            sec10_list.append(y3)
+            sec30_list.append(y2)
+            sec60_list.append(y1)
             for i in range(3):
-                average_all[3][i] =+ y1[i]
-                average_all[2][i] =+ y2[i]
-                average_all[1][i] =+ y3[i]
-                average_all[0][i] =+ y4[i]
-                average_turn2[0][i] =+ y1[i]
-                average_turn2[1][i] =+ y2[i]
-                average_turn2[2][i] =+ y3[i]
-                average_turn2[3][i] =+ y4[i]
+                average_all[3][i] += y1[i]
+                average_all[2][i] += y2[i]
+                average_all[1][i] += y3[i]
+                average_all[0][i] += y4[i]
+                average_turn2[0][i] += y1[i]
+                average_turn2[1][i] += y2[i]
+                average_turn2[2][i] += y3[i]
+                average_turn2[3][i] += y4[i]
+            
         elif(odor_time[0] == 10.0):
+            sec5_list.append(y3)
+            sec10_list.append(y1)
+            sec30_list.append(y4)
+            sec60_list.append(y2)
             for i in range(3):
-                average_all[1][i] =+ y1[i]
-                average_all[3][i] =+ y2[i]
-                average_all[0][i] =+ y3[i]
-                average_all[2][i] =+ y4[i]
-                average_turn3[0][i] =+ y1[i]
-                average_turn3[1][i] =+ y2[i]
-                average_turn3[2][i] =+ y3[i]
-                average_turn3[3][i] =+ y4[i]
+                average_all[1][i] += y1[i]
+                average_all[3][i] += y2[i]
+                average_all[0][i] += y3[i]
+                average_all[2][i] += y4[i]
+                average_turn3[0][i] += y1[i]
+                average_turn3[1][i] += y2[i]
+                average_turn3[2][i] += y3[i]
+                average_turn3[3][i] += y4[i]
         else:
+            sec5_list.append(y2)
+            sec10_list.append(y4)
+            sec30_list.append(y1)
+            sec60_list.append(y3)
             for i in range(3):
-                average_all[2][i] =+ y1[i]
-                average_all[0][i] =+ y2[i]
-                average_all[3][i] =+ y3[i]
-                average_all[1][i] =+ y4[i]
-                average_turn4[0][i] =+ y1[i]
-                average_turn4[1][i] =+ y2[i]
-                average_turn4[2][i] =+ y3[i]
-                average_turn4[3][i] =+ y4[i]
+                average_all[2][i] += y1[i]
+                average_all[0][i] += y2[i]
+                average_all[3][i] += y3[i]
+                average_all[1][i] += y4[i]
+                average_turn4[0][i] += y1[i]
+                average_turn4[1][i] += y2[i]
+                average_turn4[2][i] += y3[i]
+                average_turn4[3][i] += y4[i]
+    for i in range(4):
+        for j in range(3):
+            average_all[i][j] = average_all[i][j]/player_num
+            average_turn1[i][j] = average_turn1[i][j]/3
+            average_turn2[i][j] = average_turn2[i][j]/3
+            average_turn3[i][j] = average_turn3[i][j]/3
+            average_turn4[i][j] = average_turn4[i][j]/2
     
     subplot_area.append(fig_3part.add_subplot(3, separete, player_num+1))
     subplot_area[player_num].plot(x, average_all[0], label = "5.0", color = "red", lw =1)
     subplot_area[player_num].plot(x, average_all[1], label = "10.0", color = "blue", lw =1)
     subplot_area[player_num].plot(x, average_all[2], label = "30.0", color = "green", lw =1)
     subplot_area[player_num].plot(x, average_all[3], label = "60.0", color = "pink", lw =1)
-    subplot_area[player_num].set_title("all average")
+    subplot_area[player_num].set_title("all_average average")
     subplot_area[player_num].set_xlabel('time [minutes]')
     subplot_area[player_num].set_ylabel('saliva volume [g]')
     subplot_area[player_num].legend(loc = 'upper right')
@@ -283,14 +338,107 @@ def make_linegraph_4part(result_list, player_num, turn_list, title_list):
     plt.tight_layout()
     plt.show()
 
+    plt.rcParams["font.size"] = 15
+    labels = ['5秒', '10秒', '30秒', '60秒']
+    margin = 0.2  #0 <margin< 1
+    totoal_width = 1 - margin
+
+    #標準誤差の計算
+    error_5_before_list = []
+    error_5_now_list = []
+    error_5_after_list = []
+    error_10_before_list = []
+    error_10_now_list = []
+    error_10_after_list = []
+    error_30_before_list = []
+    error_30_now_list = []
+    error_30_after_list = []
+    error_60_before_list = []
+    error_60_now_list = []
+    error_60_after_list = []
+    for i in range(player_num):
+        error_5_before_list.append(sec5_list[i][0])
+        error_5_now_list.append(sec5_list[i][1])
+        error_5_after_list.append(sec5_list[i][2])
+        error_10_before_list.append(sec10_list[i][0])
+        error_10_now_list.append(sec10_list[i][1])
+        error_10_after_list.append(sec10_list[i][2])
+        error_30_before_list.append(sec30_list[i][0])
+        error_30_now_list.append(sec30_list[i][1])
+        error_30_after_list.append(sec30_list[i][2])
+        error_60_before_list.append(sec60_list[i][0])
+        error_60_now_list.append(sec60_list[i][1])
+        error_60_after_list.append(sec60_list[i][2])
+    sem5_before = pd.Series(error_5_before_list).sem()
+    sem5_now = pd.Series(error_5_now_list).sem()
+    sem5_after = pd.Series(error_5_after_list).sem()
+    sem10_before = pd.Series(error_10_before_list).sem()
+    sem10_now = pd.Series(error_10_now_list).sem()
+    sem10_after = pd.Series(error_10_after_list).sem()
+    sem30_before = pd.Series(error_30_before_list).sem()
+    sem30_now = pd.Series(error_30_now_list).sem()
+    sem30_after = pd.Series(error_30_after_list).sem()
+    sem60_before = pd.Series(error_60_before_list).sem()
+    sem60_now = pd.Series(error_60_now_list).sem()
+    sem60_after = pd.Series(error_60_after_list).sem()
+
+    #前も後もいる場合
+    data = [0, 0, 0]
+    x = np.array([1, 2, 3, 4])
+    for i in range(3): 
+        data[i] = average_all[0][i], average_all[1][i], average_all[2][i], average_all[3][i]
+    err = [sem5_before, sem10_before, sem30_before, sem60_before], [sem5_now, sem10_now, sem30_now, sem60_now], [sem5_after, sem10_after, sem30_after, sem60_after]
+    label = ["におい噴射の5分前", "におい噴射の直後", "におい噴射の5分後"]
+
+    #前だけいる場合
+    data = [0, 0]
+    x = np.array([1, 2, 3, 4])
+    for i in range(2): 
+        data[i] = average_all[0][i], average_all[1][i], average_all[2][i], average_all[3][i]
+    err = [sem5_before, sem10_before, sem30_before, sem60_before], [sem5_now, sem10_now, sem30_now, sem60_now]
+    label = ["におい噴射の5分前", "におい噴射の直後"]
+    
+    for i, h in enumerate(data):
+        pos = x - totoal_width *( 1- (2*i+1)/len(data))/2
+        plt.bar(pos, h, label = label[i], width = totoal_width/len(data), yerr=err[i], capsize=10)
+
+    plt.rcParams['pdf.fonttype'] = 42
+    plt.rcParams['font.family'] = 'Meiryo'
+    plt.xticks(x, labels, fontname='Meiryo', fontsize=15)
+    plt.yticks(fontname='Meiryo', fontsize=15)
+    plt.xlabel('においの噴射時間', fontname='Meiryo', fontsize=20)
+    plt.ylabel('唾液分泌量 [g]', fontname='Meiryo', fontsize=20)
+    plt.legend(loc = 'upper right', prop={"family":"Meiryo"})
+    #検定結果表示
+    addP(plt, 2, 0.3, 0.2, 0.02, "**")
+    addP(plt, 4, 0.3, 0.2, 0.02, "*")
+    plt.text(0.5, 0.48, "* : p < 0.05   ** : p < 0.01", fontname='Meiryo', fontsize=10)
+
+    plt.ylim(bottom=0, top=0.5)
+    plt.tight_layout()
+    plt.show()  
+
+    
+
+    #検定用
+    output = sec60_list
+    for i in range(player_num):
+        print(output[i][0])
+        print(output[i][1])
+        # print(output[i][2])
+
+
 def make_bargraph(result_list, player_num, turn_list, title_list):
-
     # relative_list_before2nowは1つ前からにおい噴射の変化量，relative_list_before2afterは1つ前からにおい噴射後の変化量
-
+    plt.rcParams["font.size"] = 5
     relative_list_before2now = []
     relative_list_before2after = []
     relative_list_now2after = []
     absolute_list = []
+    error5 = []
+    error10 = []
+    error30 = []
+    error60 = []
     
 
     for i in range(player_num):
@@ -337,10 +485,9 @@ def make_bargraph(result_list, player_num, turn_list, title_list):
     average_list = [0, 0, 0, 0]
 
     # data_list = absolute_list #絶対値
-    # data_list = relative_list_before2now #におい噴射直後と直前の変化量
+    data_list = relative_list_before2now #におい噴射直後と直前の変化量
     # data_list = relative_list_before2after #におい噴射直後の次と直前の変化量
-    data_list = relative_list_now2after #におい噴射直後と直後の次の変化量
-    # print(data_list)
+    # data_list = relative_list_now2after #におい噴射直後と直後の次の変化量
 
     name_anonymous = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j" ,"k"]
     for i in range(player_num):
@@ -349,37 +496,67 @@ def make_bargraph(result_list, player_num, turn_list, title_list):
 
         #検定用
         print(temp_data[0])
-        print(',')
         print(temp_data[1])
-        print(',')
         print(temp_data[2])
-        # print(temp_data[0] + ',')
-        # print(temp_data[1] + ',')
-        # print(temp_data[2] + ',')
+        print(temp_data[3])
+        error5.append(temp_data[0])
+        error10.append(temp_data[1])
+        error30.append(temp_data[2])
+        error60.append(temp_data[3])
 
         pos = x - totoal_width *( 1- (2*i+1)/player_num)/2
-        plt.bar(pos, temp_data, width = totoal_width/player_num, label = name)
-        # plt.bar(pos, temp_data, width = totoal_width/player_num, label = name_anonymous[i])
+        # plt.bar(pos, temp_data, width = totoal_width/player_num, label = name)
+        plt.bar(pos, temp_data, width = totoal_width/player_num, label = name_anonymous[i])
 
 
         for j in range(4):
             average_list[j] = average_list[j] + temp_data[j]
 
+    plt.rcParams['pdf.fonttype'] = 42
     plt.rcParams["font.family"] = 'Meiryo'   # 使用するフォント
-    plt.rcParams["font.size"] = 12                 # 文字の大きさ
+    plt.rcParams["font.size"] = 15                 # 文字の大きさ
 
     plt.xticks(x, labels, fontname='Meiryo', fontsize=20)
     # print(average_list)
     plt.yticks(fontname='Meiryo', fontsize=20)
-
-    
-    plt.plot(1, average_list[0]/player_num, '*',markersize=20)
-    plt.plot(2, average_list[1]/player_num, '*',markersize=20)
-    plt.plot(3, average_list[2]/player_num, '*',markersize=20)
-    plt.plot(4, average_list[3]/player_num, '*',markersize=20)
-    plt.legend(loc = 'upper right')
+    plt.xlabel('においの噴射時間', fontname='Meiryo', fontsize=20)
+    plt.ylabel('唾液分泌量の変化量 [g]', fontname='Meiryo', fontsize=20)
+    plt.text(1, average_list[0]/player_num, '-',horizontalalignment="center", fontname='Meiryo', fontsize=15)
+    plt.text(2, average_list[1]/player_num, '-',horizontalalignment="center", fontname='Meiryo', fontsize=15)
+    plt.text(3, average_list[2]/player_num, '-',horizontalalignment="center", fontname='Meiryo', fontsize=15)
+    plt.text(4, average_list[3]/player_num, '-',horizontalalignment="center", fontname='Meiryo', fontsize=15)
+    plt.text(0.5, 0.45, '- : 平均値', fontname='Meiryo', fontsize=15)
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0, prop={"family":"Meiryo"})
+    plt.ylim(bottom=-0.18, top=0.5)
+    # plt.legend(loc = 'upper right')
     plt.tight_layout()
     plt.show()
+
+    plt.rcParams["font.size"] = 15
+    labels = ['5秒', '10秒', '30秒', '60秒']
+    margin = 0.2  #0 <margin< 1
+    totoal_width = 1 - margin
+
+    #標準誤差の計算
+    sem5 = pd.Series(error5).sem()
+    sem10 = pd.Series(error10).sem()
+    sem30 = pd.Series(error30).sem()
+    sem60 = pd.Series(error60).sem()
+    err = sem5, sem10, sem30, sem60
+    print(err)
+    x = np.array([1, 2, 3, 4])
+
+    for i in range(4):
+        plt.bar(i+1, average_list[i]/player_num, width = totoal_width, yerr=err[i], capsize=10)
+    plt.rcParams['pdf.fonttype'] = 42
+    plt.rcParams['font.family'] = 'Meiryo'
+    plt.xticks(x, labels, fontname='Meiryo', fontsize=15)
+    plt.yticks(fontname='Meiryo', fontsize=15)
+    plt.xlabel('においの噴射時間', fontname='Meiryo', fontsize=20)
+    plt.ylabel('唾液分泌量の変化量 [g]', fontname='Meiryo', fontsize=20)
+    plt.ylim(bottom=-0.05, top=0.2)
+    plt.tight_layout()
+    plt.show()  
     
 def make_average_linegraph(result_list, player_num, turn_list, title_list):
     #result_listは1列の表示するグラフ，player_numはプレイヤーの人数，turu_listはにおいを噴射した時間，title_listはタイトル名
@@ -403,6 +580,14 @@ def make_average_linegraph(result_list, player_num, turn_list, title_list):
     plt.plot(x, y, lw=1)
     plt.legend(loc = 'upper right')
     plt.show()
+
+
+def addP(plt, p_xplace, p_yplace, p_width, p_height, p_text):
+    plt.plot([p_xplace-p_width, p_xplace + p_width],[p_yplace, p_yplace], color="black")
+    plt.plot([p_xplace-p_width, p_xplace-p_width],[p_yplace, p_yplace - p_height], color="black")
+    plt.plot([p_xplace + p_width, p_xplace + p_width],[p_yplace, p_yplace - p_height], color="black")
+    plt.text(p_xplace, p_yplace + p_height/2, p_text, horizontalalignment="center")
+    
 
 if __name__ == '__main__':
     main()
